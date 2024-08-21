a9fe28faae3bdaf358e0837934ebdadb6d6856cf
OmarAlaa12330 <omar.alaa12330@gmail.com>
2024-08-21 13:43:45 +0100

    Getting a simple shell to print its input

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    char input[1024];
+
+    while (1) {
+        printf("myshell> ");
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            break;
+        }
+
+        printf("You entered: %s", input);
+    }
+    printf("Closing shell...");
     return 0;
 }
